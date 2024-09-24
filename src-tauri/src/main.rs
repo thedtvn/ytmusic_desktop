db49068743921b2d4a4ef4847b227539f1aef0ad
thedtvn <duongtuan30306@gmail.com>
2024-09-24 17:28:50 +0700

    update activity fix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -26,18 +26,15 @@ fn create_tray() -> SystemTray {
 fn create_discord_rpc() -> UnboundedSender<PlayerState> {
     let mut drpc = discord_rpc_client::Client::new(1049275932239728672);
     drpc.start();
-    drpc.set_activity(|a| a.details("idle not playing").timestamps(|x| x.end(0))).unwrap();
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<PlayerState>();
     async_runtime::spawn(async move {
         while let Some(data) = rx.recv().await {
             let mut drpc = drpc.clone();
             let _ = std::thread::spawn(move || {
                 if data.is_distroyed {
-                    drpc.clear_activity().unwrap();
                     drpc.set_activity(|a| a.details("idle not playing").timestamps(|x| x.end(0))).unwrap();
                 } else {
                     let video_data = data.video_data.unwrap();
-                    drpc.clear_activity().unwrap();
                     drpc.set_activity(|a| {
                         let b = a
                             .instance(true)
